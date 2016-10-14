96389010b38218745032fa3cb6a59b80a9912451
blakehawkins <blake.hawkins.11@gmail.com>
2016-10-14 14:00:16 +0100

    add vscode

--- source_live_dotfiles.sh
+++ source_live_dotfiles.sh
@@ -3,4 +3,5 @@ cp ~/.vimrc     ./configs/
 cp ~/.tmux.conf ./configs/
 cp ~/.bashrc    ./configs/
 cp ~/.gitconfig ./configs/
+cp ~/.config/Code/User/settings.json ./configs/_vscode_user_settings.json
 
--- use_repo_dotfiles.sh
+++ use_repo_dotfiles.sh
@@ -3,4 +3,5 @@ cp ./configs/.vimrc     ~/
 cp ./configs/.tmux.conf ~/
 cp ./configs/.bashrc    ~/
 cp ./configs/.gitconfig ~/
+cp ./configs/_vscode_user_settings.json ~/.config/Code/User/settings.json
 
