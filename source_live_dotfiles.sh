fa94797651f29563e21ed447d17b003c51891826
blakehawkins <blake.hawkins.11@gmail.com>
2017-03-13 13:54:45 +0000

    update vscode dotfiles

--- source_live_dotfiles.sh
+++ source_live_dotfiles.sh
@@ -4,4 +4,5 @@ cp ~/.tmux.conf ./configs/
 cp ~/.bashrc    ./configs/
 cp ~/.gitconfig ./configs/
 cp ~/.config/Code/User/settings.json ./configs/_vscode_user_settings.json
+cp ~/.config/Code/User/keybindings.json ./configs/_vscode_keybindings.json
 
--- use_repo_dotfiles.sh
+++ use_repo_dotfiles.sh
@@ -4,4 +4,4 @@ cp ./configs/.tmux.conf ~/
 cp ./configs/.bashrc    ~/
 cp ./configs/.gitconfig ~/
 cp ./configs/_vscode_user_settings.json ~/.config/Code/User/settings.json
-
+cp ./configs/_vscode_keybindings.json ~/.config/Code/User/keybindings.json
