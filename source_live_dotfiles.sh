1e22903249d3e387b88da428fd9975bacd926413
blakehawkins <blake.hawkins.11@gmail.com>
2017-08-24 16:34:07 +0100

    add sublime keybindings for chrome-style tab left and tab right

--- source_live_dotfiles.sh
+++ source_live_dotfiles.sh
@@ -5,4 +5,4 @@ cp ~/.bashrc    ./configs/
 cp ~/.gitconfig ./configs/
 cp ~/.config/Code/User/settings.json ./configs/_vscode_user_settings.json
 cp ~/.config/Code/User/keybindings.json ./configs/_vscode_keybindings.json
-
+cp ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap ./configs/_sublime_keybindings.json
--- use_repo_dotfiles.sh
+++ use_repo_dotfiles.sh
@@ -5,3 +5,4 @@ cp ./configs/.bashrc    ~/
 cp ./configs/.gitconfig ~/
 cp ./configs/_vscode_user_settings.json ~/.config/Code/User/settings.json
 cp ./configs/_vscode_keybindings.json ~/.config/Code/User/keybindings.json
+cp ./configs/_sublime_keybindings.json ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap
